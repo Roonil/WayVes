4e7882c385a34a61f5f50d25e4b28a5983b01f41
Roonil <anandverma458@gmail.com>
2026-01-23 01:45:43 +0530

    Corrected  top bars config to match the showcase

--- advanced-configs/batmobile-wallpaper-extreme/linear.glsl
+++ advanced-configs/batmobile-wallpaper-extreme/linear.glsl
@@ -1,15 +1,18 @@
 #include "linear/structs.glsl"
 #include "utils/post-processing/glow/structs.glsl"
+#include "utils/post-processing/rotate/structs.glsl"
 
 #define coordinateRotation 0
 
 #define particleRadius 1
 #define fragmentWidth 4
 #define leftPadding 0
-#define rightPadding 0
+#define rightPadding 120
 #define particleBorderSize 1
 #define amplify 102.
 #define weight 3.4
+
+#define barSide 1.
 
 #define innerParticleSoftness 2.
 #define outerParticleSoftness 1.
@@ -36,8 +39,9 @@ void init()
     audioSettings.mode = 1;
     particle.audio.multiplier = amplify;
     bar.audio.multiplier = amplify;
-    bar.mergeLeftBar = 1;
-    bar.mergeRightBar = 1;
+    int barGroupCount = int(mix(mix(mix(mix(2, 4, step(bar.fragment.n, 35)), 7, step(bar.fragment.n, 24)), 8, step(bar.fragment.n, 18)), 3, step(bar.fragment.n, 4.)));
+    bar.mergeLeftBar = 1 - int(step(mod(bar.fragment.n, barGroupCount), 0));
+    bar.mergeRightBar = 1 - int(step(barGroupCount - 1, mod(bar.fragment.n, barGroupCount)));
 
     audioSettings.combineChannels = 0;
 }
@@ -94,25 +98,26 @@ void setProps()
     particle.cap.offset.y = -2.;
     particle.cap.color = vec4(1.0, 0.3961, 0.6196, 1.0);
 
-    bar.size = vec3(5., 9., 5);
-    bar.borderSize = vec3(1, 1, 1);
+    bar.size = mix(vec3(5., 9., 5), vec3(0., 12., 0), barSide);
+    bar.borderSize = mix(vec3(1, 1, 1), vec3(0, 1, 1), barSide);
 
-    bar.outerSoftness = vec3(3.3, .1, 3);
-    bar.innerSoftness = vec3(2.5, .3, 2.5);
+    bar.outerSoftness = mix(vec3(3.3, .1, 3), vec3(0, .3, .3), barSide);
+    bar.innerSoftness = mix(vec3(2.5, .3, 2.5), vec3(0, 1.3, 1.3), barSide);
 
     bar.offset.y = 95;
 
-    bar.color = mix(vec4(0.0392, 0.0118, 0.0784, 0.301), vec4(0.6627, 0.0706, 0.1686, 0.351), clamp(2. * abs(.5 - bar.fragment.coords.x / resolution.x) / 3., 0, 1));
-    bar.borderColor = vec4(1.0, 0.6706, 0.8353, 1.0);
+    bar.color = mix(mix(vec4(0.0392, 0.0118, 0.0784, 0.301), vec4(0.6627, 0.0706, 0.1686, 0.351), clamp(2. * abs(.5 - bar.fragment.coords.x / resolution.x) / 3., 0, 1)), vec4(0.0, 0.0, 0.0, 0.738), barSide);
+    bar.borderColor = mix(vec4(1.0, 0.6706, 0.8353, 1.0), vec4(1), barSide);
 
     bar.downCap.enable = true;
     bar.downCap.rate = 0.01;
     bar.downCap.elasticity = .6;
-    bar.downCap.launchVelocity = 1.1;
+    bar.downCap.launchVelocity = 0;
     bar.downCap.acceleration = .3;
-    bar.downCap.launchFlingMultiplier = 1.8;
+    bar.downCap.launchFlingMultiplier = 2.4;
     bar.downCap.size.x = 8.;
-    bar.downCap.size.y = 1.;
+    bar.downCap.size.y = mix(1., 2., barSide);
+    bar.downCap.softness = mix(vec3(0), vec3(.5), barSide);
     bar.downCap.offset = vec2(0);
     bar.downCap.color = mix(vec4(0.6471, 0.1176, 0.9294, 0.775), vec4(0.1176, 0.7608, 0.9569, 0.863), clamp(.8 * abs(bar.audio.current.x / bar.fragment.coords.y), 0, 1));
 }
@@ -140,4 +145,15 @@ void setGlow0(inout Glow glow)
     glow.quality = 3;
     glow.brightnessOffset = .4;
     glow.lightStrength = 1;
+}
+
+void setRotate0(inout Rotate rotate)
+{
+
+#define tstWt 1.3
+#define tW2 tstWt* tstWt
+#define tEW exp(-tW2)
+#define dT (1. - texture(audioR, .06).x)
+
+    rotate.coords.x += int(-120 * abs(sin(1.5 * (clamp(((exp(-dT * dT * tW2) - tEW) * (tW2)) - .01, 0.1, .8)))));
 }
